85086bd0208c952e47f82df89cf4dd145ff96e8a
Soham Ghosh <sgggz217@gmail.com>
2026-09-14 07:53:48 +0530

    bubble sort
    self made iteration idk how inefficient it is lol

---
+++ bblSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+ int main()
+ {  
+    int a,b,z=0, j=0;
+    int i=0;
+    int p[]={2, 5, 7, 3, 9, 4, 8, 1};
+    while (z <= 8){
+        while (i<=6){
+            if( p[i] > p[i+1] ){
+                a=p[i];
+                b=p[i+1];
+                p[i+1]=a;
+                p[i]=b;
+            } else {
+                p[i+1]=p[i+1];
+                p[i]=p[i];
+            }
+            i++;
+        };
+        i=0;
+        z++;
+    };
+    while(j<=7){
+        cout << p[j] << " ";
+        j++;
+    }
+ }
